9ed407b494fdb7f7740e9cdccf5671cc781fcadd
ZhengChengkai826 <zchengkai826@gmail.com>
2019-07-16 14:21:37 +0800

    add load logic p1

--- selenium/m3d_loader.cpp
+++ selenium/m3d_loader.cpp
@@ -1,4 +1,9 @@
 #include "m3d_loader.h"
+#include <fstream>
+#include <string>
+#include <DirectXMath.h>
+
+using namespace DirectX;
 
 bool M3DLoader::LoadM3d(const std::string &filename,
 	std::vector<SkinnedVertex> &vertices,
@@ -7,5 +12,30 @@ bool M3DLoader::LoadM3d(const std::string &filename,
 	std::vector<M3dMaterial> &mats,
 	SkinnedData &skinnedData) {
 	
-	return true;
+	std::ifstream fin(filename);
+
+	UINT numMaterials = 0;
+	UINT numVertices = 0;
+	UINT numTriangles = 0;
+	UINT numBones = 0;
+	UINT numAnimationClips = 0;
+
+	std::string ignore;
+
+	if (fin)
+	{
+		fin >> ignore; // file header text
+		fin >> ignore >> numMaterials;
+		fin >> ignore >> numVertices;
+		fin >> ignore >> numTriangles;
+		fin >> ignore >> numBones;
+		fin >> ignore >> numAnimationClips;
+
+		std::vector<XMFLOAT4X4> boneOffsets;
+		std::vector<int> boneHierarchy;
+		std::unordered_map<std::string, AnimationClip> animationClips;
+
+		return true;
+	}
+	return false;
 }
